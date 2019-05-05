87c1ff65049c94db036bb048ef64604e93e7230f
jerseybiomed <39709120+jerseybiomed@users.noreply.github.com>
2019-05-05 21:11:39 +0500

    Sandwich_Should

--- Environment/Ingredients/Bread.cs
+++ Environment/Ingredients/Bread.cs
@@ -2,7 +2,7 @@
 {
     public class Bread : Ingredient { }
 
-    public class Brown : Bread { public static new int MaxCountOnStorage = 3; }
+    public class BrownBread : Bread { public static new int MaxCountOnStorage = 3; }
     public class RyeBread : Bread { public static new int MaxCountOnStorage = 3; }
     public class WhiteBread : Bread{ public static new int MaxCountOnStorage = 3; }
 }
--- Tests/Sandwich_Should.cs
+++ Tests/Sandwich_Should.cs
@@ -12,13 +12,94 @@ namespace Cafe.Tests
     [TestFixture]
     class Sandwich_Should
     {
+        private readonly Sandwich sandwich = new Sandwich();
+        private readonly BrownBread brown = new BrownBread();
+        private readonly RyeBread rye = new RyeBread();
+        private readonly WhiteBread white = new WhiteBread();
+        private readonly Beef beef = new Beef();
+        private readonly Hon hon = new Hon();
+        private readonly Pork pork = new Pork();
+        private readonly Tomato tomato = new Tomato();
+        private readonly Cucumber cucumber = new Cucumber();
+        private readonly Salad salad = new Salad();
+
         [Test]
-        public void CreationTest()
+        public void FirstAddBread()
+        {
+            sandwich.Add(rye);
+            Assert.AreEqual(sandwich.Bread, rye);
+        }
+
+        [Test]
+        public void SecondAddMeat()
+        {
+            sandwich.Add(beef);
+            Assert.AreEqual(sandwich.Meat, beef);
+        }
+
+        [Test]
+        public void ThirdAddVegetables()
+        {
+            sandwich.Add(tomato);
+            Assert.AreEqual(sandwich.Vegetables, tomato);
+        }
+
+        [Test]
+        public void WrongIngredientsOrder1()
         {
             var sandwich = new Sandwich();
-            sandwich.Add(new Bread());
-            sandwich.Add(new Meat());
-            sandwich.Add(new Vegetables());
+            sandwich.Add(salad);
+            Assert.AreEqual(sandwich.Vegetables, null);
+        }
+
+        [Test]
+        public void WrongIngredientsOrder2()
+        {
+            var sandwich = new Sandwich();
+            sandwich.Add(pork);
+            Assert.AreEqual(sandwich.Meat, null);
+        }
+
+        [Test]
+        public void WrongIngredientsOrder3()
+        {
+            var sandwich = new Sandwich();
+            sandwich.Add(brown);
+            sandwich.Add(salad);
+            Assert.AreEqual(sandwich.Vegetables, null);
+        }
+
+        [Test]
+        public void CanNotAddSecondBread()
+        {
+            var sandwich = new Sandwich();
+            sandwich.Add(brown);
+            sandwich.Add(white);
+            Assert.AreNotEqual(sandwich.Bread, null);
+            Assert.AreNotEqual(sandwich.Bread, white);
+        }
+
+        [Test]
+        public void CanNotAddSecondMeat()
+        {
+            var sandwich = new Sandwich();
+            sandwich.Add(rye);
+            sandwich.Add(pork);
+            sandwich.Add(hon);
+            Assert.AreNotEqual(sandwich.Meat, null);
+            Assert.AreNotEqual(sandwich.Meat, hon);
+        }
+
+        [Test]
+        public void CanNotAddSecondVegetables()
+        {
+            var sandwich = new Sandwich();
+            sandwich.Add(white);
+            sandwich.Add(hon);
+            sandwich.Add(salad);
+            sandwich.Add(cucumber);
+            Assert.AreNotEqual(sandwich.Vegetables, null);
+            Assert.AreNotEqual(sandwich.Vegetables, cucumber);
         }
     }
 }
